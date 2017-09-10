4aa7f1bdef1a8d38a48d5d2b5fb4dd9e6d68455c
goran <goran.brlas@gmail.com>
2017-09-10 21:25:44 +0200

    Word guessing game created for iPads.

---
+++ Apple Pie/Apple Pie/Game.swift
@@ -0,0 +1,36 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Goran Brlas on 10/09/2017.
+//  Copyright © 2017 Goran Brlas. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    var formattedWord: String {
+        var guessedWord = ""
+        
+        for letter in word.characters {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        
+        return guessedWord
+    }
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        
+        if !word.characters.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+}
---
+++ Apple Pie/Apple Pie/ViewController.swift
@@ -0,0 +1,97 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by Goran Brlas on 10/09/2017.
+//  Copyright © 2017 Goran Brlas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    
+    var listOfWords = ["barcelona", "mordor", "messi", "xenoverse", "fifa"]
+    
+    let incorrectMovesAllowed = 7
+    var currentGame: Game!
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newRound()
+    }
+    
+    func newRound() {
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        } else {
+            enableLetterButtons(false)
+        }
+    }
+    
+    func enableLetterButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
+    }
+    
+    func updateUI() {
+        let letters = currentGame.formattedWord.characters.map{String($0)}
+        
+        correctWordLabel.text = letters.joined(separator: " ")
+        
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
+    
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+        
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        
+        currentGame.playerGuessed(letter: letter)
+        updateGameState()
+    }
+    
+    
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
