ccef2331df37d948abeec42fd46af328b8599ce1
goran <goran.brlas@gmail.com>
2017-09-10 17:06:15 +0200

    App with a simple calculator UI, no real functionality yet..

---
+++ Calculator2/Calculator2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator2
+//
+//  Created by Goran Brlas on 10/09/2017.
+//  Copyright © 2017 Goran Brlas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
