a888e1250534e4a93a600886639cddf190a422f2
goran <goran.brlas@gmail.com>
2017-09-26 14:07:34 +0200

    Slight changes in the storyboard.

---
+++ AppLifeCycle/AppLifeCycle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppLifeCycle
+//
+//  Created by Goran Brlas on 26/09/2017.
+//  Copyright © 2017 Goran Brlas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
