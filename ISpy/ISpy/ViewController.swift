19bfc65dc02a9664b539d49372d8bdb7a38f8a50
goran <goran.brlas@gmail.com>
2017-09-28 22:31:00 +0200

    Added a simple app with image zooming.

---
+++ ISpy/ISpy/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  ISpy
+//
+//  Created by Goran Brlas on 28/09/2017.
+//  Copyright © 2017 Goran Brlas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.delegate = self
+        updateZoomFor(size: view.bounds.size)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func updateZoomFor(size: CGSize) {
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.height / imageView.bounds.height
+        let scale = min(widthScale, heightScale)
+        scrollView.minimumZoomScale = scale
+    }
+
+}
+
