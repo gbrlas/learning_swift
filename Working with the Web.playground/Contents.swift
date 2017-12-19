717a5ce76dcb5b97c7323c7204a611cfe3afc6bc
goran <goran.brlas@gmail.com>
2017-12-19 21:26:04 +0100

    Added playground with URL manipulation.

---
+++ Working with the Web.playground/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+extension URL {
+    func withQueries(_ queries: [String: String]) -> URL? {
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        components?.queryItems = queries.flatMap({ URLQueryItem(name: $0.0, value: $0.1)
+        })
+        
+        return components?.url
+    }
+}
+
+let query: [String: String] = [
+    "api_key": "DEMO_KEY",
+    "date" : "2011-07-13"
+]
+
+let baseURL = URL(string: "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")!
+let url = baseURL.withQueries(query)!
+let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+    if let data = data,
+        let string = String(data: data, encoding: .utf8) {
+            print(string)
+    }
+}
+
+print(url.absoluteURL)
+
+task.resume()
